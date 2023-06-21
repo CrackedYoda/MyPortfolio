98cfd50670e189b687795cdf645a91207f0aa157
Crackedyoda <nelsonakalia12@gmail.com>
2023-06-21 10:40:48 +0100

    fix linters

--- menu.js
+++ menu.js
@@ -1,16 +1,44 @@
-
 const hamburger = document.querySelector('.nav-menu');
+const navBar = document.querySelector('.navbar');
+const navlist = document.querySelector('.nav-list');
+const portfolio = document.getElementById('portfoliolink');
+const about = document.getElementById('aboutlink');
+const contact = document.getElementById('contactlink');
 
-hamburger.addEventListener ('click', () => {
-    let navBar = document.querySelector('.navbar');
-    navBar.classList.toggle('active');
-    navBar.style.backgroundColor = '#6070FF';
-    document.querySelector('.nav-bar').classList.toggle('activecolor');
-    hamburger.classList.toggle('cancel');
-    
-    let navlist = document.querySelector('.nav-list');
-    navlist.classList.toggle('active');
-    document.querySelector('.logo').classList.toggle('inactive');
-} 
-);
+hamburger.addEventListener('click', () => {
+  navBar.classList.toggle('active');
+  document.querySelector('.nav-bar').classList.toggle('activecolor');
+  hamburger.classList.toggle('cancel');
+  document.querySelector('.headline').classList.toggle('blur');
+  document.querySelector('.work').classList.toggle('blur');
+  document.querySelector('.about').classList.toggle('blur');
+  document.querySelector('.form').classList.toggle('blur');
+  navlist.classList.toggle('activelist');
+  document.querySelector('.logo').classList.toggle('inactive');
+});
 
+portfolio.addEventListener('click', () => {
+  navBar.classList.remove('active');
+  hamburger.classList.remove('cancel');
+  document.querySelector('.nav-bar').classList.remove('activecolor');
+  document.querySelector('.headline').classList.remove('blur');
+  navlist.classList.remove('activelist');
+  document.querySelector('.logo').classList.remove('inactive');
+});
+about.addEventListener('click', () => {
+  navBar.classList.remove('active');
+  hamburger.classList.remove('cancel');
+  document.querySelector('.nav-bar').classList.remove('activecolor');
+  document.querySelector('.headline').classList.remove('blur');
+  navlist.classList.remove('activelist');
+  document.querySelector('.logo').classList.remove('inactive');
+});
+
+contact.addEventListener('click', () => {
+  navBar.classList.remove('active');
+  hamburger.classList.remove('cancel');
+  document.querySelector('.nav-bar').classList.remove('activecolor');
+  document.querySelector('.headline').classList.remove('blur');
+  navlist.classList.remove('activelist');
+  document.querySelector('.logo').classList.remove('inactive');
+});
