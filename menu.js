d044b8c96ba6d0397ac56ca933f73a3d5eadafdf
Crackedyoda <nelsonakalia12@gmail.com>
2023-06-24 09:18:58 +0100

    add styling

--- menu.js
+++ menu.js
@@ -4,6 +4,7 @@ const navlist = document.querySelector('.nav-list');
 const portfolio = document.getElementById('portfoliolink');
 const about = document.getElementById('aboutlink');
 const contact = document.getElementById('contactlink');
+const seeproject = document.getElementsByClassName('.seeproject');
 
 hamburger.addEventListener('click', () => {
   navBar.classList.toggle('active');
@@ -48,3 +49,47 @@ about.addEventListener('click', () => {
   navlist.classList.remove('activelist');
   document.querySelector('.logo').classList.remove('inactive');
 });
+
+
+// pop up section
+ 
+// pop up text
+const projects = [
+    {
+      name: 'tonic',
+      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      image: 'images/tonic.png',
+      technologies: ['HTML', 'CSS', 'javascript'],
+      liveLink: 'https://www.google.com',
+      sourceLink: 'https://www.github.com/Akorede3133/portfolio',
+      id: 'tonic',
+    },
+    {
+      name: 'multi-Post stories',
+      description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+      image: 'images/multi-stories.png',
+      technologies: ['HTML', 'Ruby on rails', 'CSS', 'javascript'],
+      liveLink: 'https://www.google.com',
+      sourceLink: 'https://www.github.com/Akorede3133/portfolio',
+      id: 'stories',
+    },
+    {
+      name: 'facebook 360',
+      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      image: 'images/facebook360.png',
+      technologies: ['HTML', 'CSS', 'javascript'],
+      liveLink: 'https://www.google.com',
+      sourceLink: 'https://www.github.com/Akorede3133/portfolio',
+      id: 'facebook',
+    },
+    {
+      name: 'uber navigation',
+      description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+      image: 'images/uber.png',
+      technologies: ['HTML', 'Ruby on rails', 'CSS', 'javascript'],
+      liveLink: 'https://www.google.com',
+      sourceLink: 'https://www.github.com/Akorede3133/portfolio',
+      id: 'uber',
+    },
+  ];
+
